816131eff7419ca595ea6fc5a66609e6420a19f0
Thales Pereira <thalesfp@gmail.com>
2020-03-30 09:15:52 -0700

    Use serverless webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: "production",
+  externals: [nodeExternals()],
+};
