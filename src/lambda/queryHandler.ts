3b3633b40a1192c885304472d9d5a3b444426085
Thales Pereira <thalesfp@gmail.com>
2020-08-02 15:30:20 -0700

    Fix queryHandler param names

--- src/lambda/queryHandler.ts
+++ src/lambda/queryHandler.ts
@@ -7,81 +7,53 @@ import { getYearModels } from "../repository/yearModels";
 import { numberToVehicleType } from "../transformers/valuesToRemoteApi";
 
 export const queryCurrentReference: APIGatewayProxyHandler = async () => {
-  try {
-    const currentReference = await getCurrentReference();
+  const currentReference = await getCurrentReference();
 
-    return {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(currentReference),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error,
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(currentReference),
+  };
 };
 
 export const queryBrands: APIGatewayProxyHandler = async (event) => {
-  try {
-    const vehicleTypeParam = event.pathParameters?.vehicleType;
-    if (!vehicleTypeParam) throw Error("Parameter missing: vehicleType");
+  const vehicleTypeParam = event.pathParameters?.vehicleType;
+  if (!vehicleTypeParam) throw Error("Parameter missing: vehicleType");
 
-    const vehicleType = numberToVehicleType(parseInt(vehicleTypeParam, 10));
-    const brands = await getBrands(vehicleType);
+  const vehicleType = numberToVehicleType(parseInt(vehicleTypeParam, 10));
+  const brands = await getBrands(vehicleType);
 
-    return {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(brands),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error,
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(brands),
+  };
 };
 
 export const queryModels: APIGatewayProxyHandler = async (event) => {
-  try {
-    const brandIdParam = event.pathParameters?.brandIdParam;
-    if (!brandIdParam) throw Error("Parameter missing: brandIdParam");
+  const brandIdParam = event.pathParameters?.brandId;
+  if (!brandIdParam) throw Error("Parameter missing: brandId");
 
-    const brandId = parseInt(brandIdParam, 10);
-    const models = await getModels(brandId);
+  const brandId = parseInt(brandIdParam, 10);
+  const models = await getModels(brandId);
 
-    return {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(models),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error,
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(models),
+  };
 };
 
 export const queryYearModels: APIGatewayProxyHandler = async (event) => {
-  try {
-    const modelIdParam = event.pathParameters?.modelIdParam;
-    if (!modelIdParam) throw Error("Parameter missing: brandIdParam");
+  const modelIdParam = event.pathParameters?.modelId;
+  if (!modelIdParam) throw Error("Parameter missing: brandId");
 
-    const modelId = parseInt(modelIdParam, 10);
-    const yearModels = await getYearModels(modelId);
+  const modelId = parseInt(modelIdParam, 10);
+  const yearModels = await getYearModels(modelId);
 
-    return {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(yearModels),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error,
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(yearModels),
+  };
 };
